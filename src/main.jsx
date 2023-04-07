27a053e04ebba33ecd1b709276dc9a8da1be540d
jahirul94 <jahirulislam41185@gmail.com>
2023-04-07 15:47:43 +0600

    added loader data

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import NavBar from './component/NavBar/NavBar';
+import Main from './component/LayOut/Main';
+import Home from './component/Home/Home';
+import OrderReview from './component/OrderReview/OrderReview';
+
+const router = createBrowserRouter([
+  {
+     path : '/',
+     element : <Main></Main>,
+     children :[
+     {
+      path :'/',
+      element : <Home></Home>,
+      loader : ()=> fetch ('t-shirts.json')
+     },
+     {
+      path : 'review',
+      element : <OrderReview></OrderReview>
+     }
+
+     ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
